f228435fa714654c5b82227ceb9bec8600e861a8
denis909 <denis909@mail.ru>
2019-09-04 19:41:57 +0300

    n/a

---
+++ GlyphiconsAsset.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace denis909\yii\glyphicons;
+
+use yii\web\AssetBundle;
+
+class GlyphiconsAsset extends AssetBundle
+{
+
+    public $sourcePath = '@bower/glyphicons';
+    
+    public $css = ['styles/glyphicons.css'];
+
+}
